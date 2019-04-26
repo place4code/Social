296becc8d00d9705d273b65be3a4b976b385bb45
place4code <place4code@gmail.com>
2019-04-26 11:10:33 +0200

    add security config file and entity User

---
+++ src/main/java/com/place4code/social/entity/User.java
@@ -0,0 +1,59 @@
+package com.place4code.social.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String username;
+	
+	private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public User(int id, String username, String email) {
+		this.id = id;
+		this.username = username;
+		this.email = email;
+	}
+
+	public User() {
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", email=" + email + "]";
+	}
+	
+	
+
+}
